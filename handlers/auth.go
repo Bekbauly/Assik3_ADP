61561187d0a63e116d066cd9d4581b8283303434
igiiaw <zhnamantai@gamail.com>
2026-01-25 23:44:39 +0500

    add Login handler

--- handlers/auth.go
+++ handlers/auth.go
@@ -1,1 +1,16 @@
 package handlers
+
+import (
+	"encoding/json"
+	"net/http"
+)
+
+// Login plug
+func Login(w http.ResponseWriter, r *http.Request) {
+	response := map[string]string{
+		"token": "mock-jwt-token-12345",
+		"user":  "student@uni.edu",
+	}
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(response)
+}
